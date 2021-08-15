4fd64710a0583b7e4650c7b4e52c74e3564955f8
rudraraval <76518990+rudraraval@users.noreply.github.com>
2021-08-15 15:59:57 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,33 @@
+class Paper
+    {
+        constructor(x,y, r, angle){
+            var options={ 
+                isStatic: false,
+                restitution:0.3,
+                friction:0,
+                density:1.2
+            }
+            this.x=x;
+            this.y= y;
+            this.r= r;
+            this.angle= angle;
+            this.image= loadImage("paper.png")
+            this.body= Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+            World.add(world, this.body)
+        }
+
+        display(){
+            var pos= this.body.position
+            push()
+
+            translate(pos.x, pos.y)
+            rotate(this.angle)
+            fill("grey")
+            imageMode(CENTER)
+            image(this.image, 0,0 , this.r, this.r)
+
+            pop()
+                
+            
+        }
+    }
